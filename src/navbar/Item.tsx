3eb104afe00278e13110667ce7de76db2d2f2d03
Hmducc <hmd.2k3@gmail.com>
2024-08-26 19:39:04 +0700

    Test responsive homepage

--- src/navbar/Item.tsx
+++ src/navbar/Item.tsx
@@ -1,8 +1,8 @@
 // src/components/ItemCard.tsx
-import React from "react";
-import "./Item.css";
-import { Link } from "react-router-dom";
 import { Typography } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Item.css";
 
 interface ItemProps {
   image: string;
@@ -14,13 +14,17 @@ const { Text } = Typography;
 const Item: React.FC<ItemProps> = ({ image, name, price, id }) => {
   return (
     <Link to={`/details/${id}`} className="item-card-link">
-      <div className="item-card ">
-        <img src={image} alt={name} className="item-image" />
+      <div className="item-card lg:w-80 lg:h-80 md:h-40 md:w-40 ipad-pro:h-60 ipad-pro:w-60   ">
+        <img
+          src={image}
+          alt={name}
+          className="  item-image lg:w-96 lg:h-60 md:w-48 md:h-30 lg:mt-0   "
+        />
         <div className="item-details ">
-          <Text ellipsis className="item-name barlow3">
+          <Text ellipsis className="item-name lg:text-lg barlow3 lg:w-50 w-25">
             {name}
           </Text>
-          <p className="item-price barlow3">{price}</p>
+          <p className="item-price barlow3 lg:text-lg">{price}</p>
         </div>
       </div>
     </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        'ipad-pro': '1024px',
+        'lg': '1100px',
+        // => @media (min-width: 1280px) { ... }
+      },
       colors: {
         main: "#EA1B25",
+        border: "#ddd"
       },
+      spacing: {
+        '500': '32rem',
+        '700': '50rem',
+        '75': '18.75rem',
+        '25': '6.25rem',
+        '50': '12.5rem',
+        '1': '0.063rem',
+        '5.5': '1.406rem',
+      }
     },
   },
   plugins: [],
