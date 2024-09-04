5ac902d01efa889556e417327f65ec60b755cc24
Hmducc <hmd.2k3@gmail.com>
2024-09-04 18:15:44 +0700

    Responsive Details.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '40': '2.5rem',
+    },
     extend: {
       screens: {
         'ipad-pro': '1024px',
@@ -26,6 +36,8 @@ export default {
         '1500': '93.75rem',
         '1000': '62.5rem',
         '1300': '81.25rem',
+        '400': '25rem',
+        '90': '5.625rem',
       }
     },
   },
